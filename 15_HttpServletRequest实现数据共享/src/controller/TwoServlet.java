2859fd5cdd312fbc01f4ba6ae76df82e4e4ebc98
lsl <1215288388@qq.com>
2021-09-06 08:41:58 +0800

    HttpServletRequest实现数据共享

---
+++ 15_HttpServletRequest实现数据共享/src/controller/TwoServlet.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TwoServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //从同一个请求作用域对象得到OneServlet写入到共享数据
+        String value = (String)request.getAttribute("key1");
+        System.out.println("TwoServlet得到共享数据 "+value);
+    }
+}
